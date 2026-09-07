4f80d0dd6da307ae3d07d9f4817900b3f9d9a5d8
Aayushi Pujara <aayupujara80@gmail.com>
2026-09-07 14:30:51 +0530

    Update prog1.c

--- unit3/prog1.c
+++ unit3/prog1.c
@@ -112,6 +112,120 @@ void display()
     }
 }
 
+void insert_beg()
+{
+   struct node *new_node;
+   int num;
+   printf("\n Enter the data :");
+   scanf("%d", &num);
+   new_node = (struct node *)malloc(sizeof(struct node));
+   new_node ->data=num;
+   new_node ->next=start;
+   start=new_node;
+}
+
+void insert_end()
+{
+   struct node *ptr, *new_node;
+   int num;
+   printf("\n Enter the data :");
+   scanf("%d", &num);
+   new_node = (struct node *)malloc(sizeof(struct node));
+   new_node ->data=num;
+   new_node ->next=NULL;
+   ptr=start;
+   while(ptr -> next !=NULL);
+      ptr = ptr -> next;
+      ptr -> next = new_node;
+}
+
+void insert_before()
+{
+   struct node*new_node, *ptr, *preptr;
+   int num,val;
+   printf("\n Enter the data :");
+   scanf("%d", &num);
+   printf("\n Enter the value before which the data has to be inserted :");
+   scanf("%d",&val);
+   new_node = (struct node *)malloc(sizeof(struct node));
+   new_node ->data=num;
+   ptr=start;
+   while(ptr -> data !=val);
+   {
+       preptr=ptr;
+       ptr = ptr -> next;
+   }
+   preptr -> next = new_node;
+   new_node -> next= ptr;
+}
+
+void insert_after()
+{
+   struct node*new_node, *ptr, *preptr;
+   int num,val;
+   printf("\n Enter the data :");
+   scanf("%d", &num);
+   printf("\n Enter the value before which the data has to be inserted :");
+   scanf("%d",&val);
+   new_node = (struct node *)malloc(sizeof(struct node));
+   new_node ->data = num;
+   ptr = start;
+   preptr=ptr;
+   while(preptr -> data !=val);
+   {
+       preptr=ptr;
+       ptr = ptr -> next;
+   }
+   preptr -> next = new_node;
+   new_node -> next= ptr;
+}
+
+void delete_beg()
+{
+  struct node *ptr;
+  ptr = start;
+  start = start ->next;
+  free(ptr);
+
+}
+
+void delete_end()
+{
+  struct node *ptr, *preptr;
+  ptr = start;
+  while(ptr -> next !=NULL);
+  {
+      preptr = ptr;
+      ptr = ptr ->next;
+  }
+  preptr -> next !=NULL;
+  free(ptr);
+
+}
+
+void delete_node()
+{
+  struct node *ptr, *preptr;
+  int val;
+  printf("\n Enter the value of the node which has to be deleted :");
+  scanf("%d",&val);
+  ptr = start;
+  if(ptr -> data==val)
+  {
+      delete_beg();
+  }
+  else
+  {
+       while(ptr -> data !=val);
+       {
+           preptr = ptr;
+           ptr = ptr -> next;
+       }
+       preptr -> next = ptr -> next;
+       free(ptr);
+  }
+}
+
 
 
 
